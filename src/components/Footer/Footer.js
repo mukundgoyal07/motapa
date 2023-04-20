448af85796c4b5476323084cb0cfa40ec3c35431
priyammedhi <priyamkar23@gmail.com>
2023-04-20 22:15:28 +0530

    1stcommit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p className="copyright">
+        Copyright &copy; 2023{" "}
+        <span>
+          Powered by: <b className="food">Priyam Medhi</b>
+        </span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
+
